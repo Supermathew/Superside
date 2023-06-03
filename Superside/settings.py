4c3e3976b79e38c0d4bdfb3811205a170d2f1bc8
Supermathew <dolikemathewalex@gmail.com>
2023-06-03 20:20:27 +0530

    superside first commit railway changes 2

--- Superside/settings.py
+++ Superside/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-"django-insecure-kn_k(!%)tgz9bg_&&1#iv48fxre%*isp*4%^b5&v0j^h=%=p^a"
+# django-insecure-kn_k(!%)tgz9bg_&&1#iv48fxre%*isp*4%^b5&v0j^h=%=p^a
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
