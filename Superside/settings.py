04302fd8d8e3afbcf938eec0cce8fb8c631bb5e3
Supermathew <dolikemathewalex@gmail.com>
2023-07-05 19:43:06 +0530

    render transfer-ALLOWED HOST-2

--- Superside/settings.py
+++ Superside/settings.py
@@ -34,7 +34,6 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-https://superside.onrender.com/
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","superside.onrender.com","http://superside.onrender.com","https://superside.onrender.com"]
 
