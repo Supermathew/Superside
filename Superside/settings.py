13dbd6761a85bd2f7486e3429c3eb6b57da39704
Supermathew <dolikemathewalex@gmail.com>
2023-07-18 21:12:25 +0530

    static vercel

--- Superside/settings.py
+++ Superside/settings.py
@@ -213,8 +213,11 @@ DATABASES ={
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # SIMPLE_JWT = {
 #     'ROTATE_REFRESH_TOKENS' : True,
 # }
